4b240b0c49e76539d734aefa3da2456f746e622d
Fero Ezar Felix <ferofelix123@gmail.com>
2023-03-08 09:42:28 +0700

    ntaps

--- latihan01/index.php
+++ latihan01/index.php
@@ -2,7 +2,25 @@
     echo "Hello world";
 print '<b>oekay</b>';
 
+    echo "<br>";
 
+$nama1 = 'Fero';
+$nama2 = "Ezar";
+    echo $nama1." ".$nama2;
+    echo "<hr>";
 
-$nama1 = 'ani';
+$nquiz = 81;
+$ntugas = 25;
+$hasil1 = $nquiz + $ntugas;
+    echo "penjumlahan : ".$hasil1;
+$hasil2 = $nquiz * $ntugas;
+    echo "<br>Perkalian : ".$hasil2;
+$hasil3 = $nquiz - $ntugas;
+    echo "<br>Pengurangan : ".$hasil3;
+$hasil4 = $nquiz / $ntugas;
+    echo "<br>Pembagian : ".round($hasil4);
+    // round pembulatan
+$hasil5 = $nquiz % $ntugas;
+    echo "<br>Exponen : ".$hasil2**2;
+    echo ", Modulus : ".$hasil5;
 ?>
