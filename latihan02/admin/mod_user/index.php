4e7f04d51c027555211a8087b1d8561efb378518
Fero Ezar Felix <ferofelix123@gmail.com>
2023-04-12 08:34:43 +0700

    new

--- latihan02/admin/mod_user/index.php
+++ latihan02/admin/mod_user/index.php
@@ -1,65 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-
-</head>
-<body>
 <form action="#" method="get">
-  <div>
-    <div class="mb-3 row">
+  <section class="container-fluid">
+    <div class="mb-3 row" style="margin-top: 30px;">
       <div class="col-md-2"></div>
       <div class="col-md">
         <div class="mb-3 row">
-          <h1>Form Input Data</h1>
+          <div class="col-md">
+            <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btnradio3">Tambah Data</label>
+          </div>          
         </div>
         <div class="mb-3 row">
-          <div class="col-md-2">
-            id_user :
-          </div>
           <div class="col-md">
-            <div class="mb-3">
-              <input type="text" class="form-control" id="exampleInputPassword1" maxlength="11">
-            </div>
-          </div>
-        </div>
-        <div class="mb-3 row">
-          <div class="col-md-2">
-            username :
-          </div>
-          <div class="col-md">
-            <div class="mb-3">
-              <input type="text" class="form-control" id="exampleInputPassword1" maxlength="16">
-            </div>
-          </div>
-        </div>
-        <div class="mb-3 row">
-          <div class="col-md-2">
-            password :
-          </div>
-          <div class="col-md">
-          <div class="mb-3">
-            <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">is_active</label>
-          </div>
-          <button type="reset" class="btn btn-secondary"><i class="bi bi-x-lg"></i> Batal</button>
-          <button type="submit" class="btn btn-primary"><i class="bi bi-save"></i> Submit</button>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Password</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mbolang</td>
+                <td>
+                  <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-check2-square"></i>Edit</label>
+                  <input type="reset" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-trash"></i>Delete</label>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Kuliah</td>
+                <td>
+                  <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-check2-square"></i>Edit</label>
+                  <input type="reset" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-trash"></i>Delete</label>
+                </td>
+              </tr>
+            </tbody>
+          </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4"></div>
+      <div class="col-md-2"></div>
     </div>
-  </div>
+  </section>
 </form>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
