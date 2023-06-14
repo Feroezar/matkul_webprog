5923ea3402c6966e8362beb7f0f00326513ca02d
Fero Ezar Felix <114554058+Feroezar@users.noreply.github.com>
2023-06-14 12:04:02 +0700

    new

--- latihanlogin/checklogin.php
+++ latihanlogin/checklogin.php
@@ -3,7 +3,7 @@ require_once("koneksi_db.php");
 session_start();
 $usernya = $_POST["txt_user"];
 $passnya = md5($_POST["txt_pasw"]);
-$query_login = mysqli_query($koneksi_db, "SELECT * FROM mst_user WHERE password = '".$passnya."' AND BINARY username = '".$usernya."'");
+$query_login = mysqli_query($koneksi_db, "SELECT * FROM mst_user WHERE password = '".$passnya."' AND BINARY username = '".$usernya."' and is_active = 1");
 $checkhasil = mysqli_num_rows($query_login);
 $nama = mysqli_fetch_array($query_login);
 if($checkhasil > 0){
